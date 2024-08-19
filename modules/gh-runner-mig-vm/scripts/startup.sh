c21b99d655c66b92555bd027a5dc88c60ad23593
Alexander Jelinek <jelinek@walter-group.com>
2024-08-19 10:05:25 +0200

    fix NVM_DIR reference

--- modules/gh-runner-mig-vm/scripts/startup.sh
+++ modules/gh-runner-mig-vm/scripts/startup.sh
@@ -26,7 +26,7 @@ setup_additional_packages () {
     } >> /root/.ssh/known_hosts
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
     # shellcheck disable=SC1090
-    NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
     export NVM_DIR
     # shellcheck disable=SC1091
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
